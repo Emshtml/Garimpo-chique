4eacde49976e1a082376b473fa8d7b8d876a346e
Emshtml <casadathereza1@gmail.com>
2025-10-22 16:58:01 -0300

    Create 404.js

---
+++ pages/pages/404.js
@@ -0,0 +1,8 @@
+export default function Custom404() {
+  return (
+    <div style={{ textAlign: 'center', padding: '2rem' }}>
+      <h1>404 - Página não encontrada</h1>
+      <p>Volte para <a href="/">a página inicial</a>.</p>
+    </div>
+  );
+}
