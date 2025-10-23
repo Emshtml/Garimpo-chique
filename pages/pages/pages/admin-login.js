72088bc65b8fdd6b7366e36a44bc541e3c5201ca
Emshtml <casadathereza1@gmail.com>
2025-10-23 10:48:50 -0300

    Create admin-login.js

---
+++ pages/pages/pages/admin-login.js
@@ -0,0 +1,12 @@
+export default function AdminLogin() {
+  return (
+    <div>
+      <h2>Login do Administrador</h2>
+      <form>
+        <input type="text" placeholder="Usuário" /><br />
+        <input type="password" placeholder="Senha" /><br />
+        <button>Entrar</button>
+      </form>
+    </div>
+  );
+}
